583762e844a7a85bfaaf778da6ff9626c14344a4
alfo0924 <146694784+alfo0924@users.noreply.github.com>
2024-08-19 23:23:02 +0800

    v1.213
    add estimator btn function cnp

--- app/src/main/java/com/example/asapelectrocountingapplicationproj/MainActivity.java
+++ app/src/main/java/com/example/asapelectrocountingapplicationproj/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.asapelectrocountingapplicationproj;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +18,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // 找到電費估算器按鈕
+        Button estimatorButton = findViewById(R.id.estimatorButton);
+
+        // 設置按鈕點擊監聽器
+        estimatorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 創建一個Intent來啟動ElectroEstimator活動
+                Intent intent = new Intent(MainActivity.this, ElectroEstimator.class);
+                startActivity(intent);
+            }
         });
     }
 }
